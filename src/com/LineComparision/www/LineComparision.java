da15c3465667152df480389d437314b4d15fa64c
soubansukri <souban.sukri@gmail.com>
2022-06-11 18:21:31 +0530

    Added:Logic for to check equality of 2 lines

--- src/com/LineComparision/www/LineComparision.java
+++ src/com/LineComparision/www/LineComparision.java
@@ -10,6 +10,8 @@ public class LineComparision {
 		System.out.println("Welcome to Line Comparison Computation Program");
 		double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		System.out.println("Length of line1: " + length1);
+		double length2 = Math.sqrt(Math.pow(x3 - x4, 2) + Math.pow(y3 - y4, 2));
+		System.out.println("Length of line2: " + length2);
 
 	}
 }
